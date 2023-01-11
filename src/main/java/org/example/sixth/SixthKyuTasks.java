04757ea4de2e2db06905cd14b2b6732b929ff0ad
memphis35 <aa.smirnow@outlook.com>
2023-01-11 11:58:29 -0600

    solve 2 regexp tasks

--- src/main/java/org/example/sixth/SixthKyuTasks.java
+++ src/main/java/org/example/sixth/SixthKyuTasks.java
@@ -1,5 +1,7 @@
 package org.example.sixth;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SixthKyuTasks {
@@ -9,9 +11,40 @@ public class SixthKyuTasks {
      * passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
      * Note: If the number is a multiple of both 3 and 5, only count it once.
      */
-    public int solution(int number) {
+    public static int solution(int number) {
         return IntStream.iterate(number, n -> n > 0, n -> n - 1)
                 .filter(n -> n % 3 == 0 || n % 5 == 0)
                 .sum();
     }
+
+    /**
+     * Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers
+     * in the form of a phone number.
+     */
+    public static String createPhoneNumber(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
+    }
+
+    /**
+     * In this kata you are given a string for example: "example(unwanted thing)example"
+     * Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+     * The example above would return: "exampleexample"
+     */
+    public static String removeParentheses(final String str) {
+        final AtomicInteger counter = new AtomicInteger(0);
+        return str.chars()
+            .filter(ch -> {
+                final boolean isOpened = ch == '(';
+                final boolean isClosed = ch == ')';
+                final int currentState = counter.addAndGet(isOpened ? 1 : (isClosed ? -1 : 0));
+                return currentState == 0 && !isOpened && !isClosed;
+            })
+            .mapToObj(Character::toString)
+            .collect(Collectors.joining());
+    }
+
+    public static String removeParentheses_2(final String str) {
+        return str.contains("(") ? removeParentheses_2(str.replaceAll("\\([^()]*\\)", "")) : str;
+    }
+
 }
